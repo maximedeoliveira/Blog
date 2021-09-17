b60b2670617a54bc3c00f36dd119540ada92ec34
Maxime <maxime.deoliveira2509@gmail.com>
2021-09-17 21:17:48 +0200

    feat: :sparkles: Set up apollo client

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,26 @@
-import { createApp } from 'vue';
+import { createApp, provide, h } from 'vue';
+import {
+    ApolloClient,
+    createHttpLink,
+    InMemoryCache,
+} from '@apollo/client/core';
+import { DefaultApolloClient } from '@vue/apollo-composable';
 import App from './App.vue';
 import router from './router';
 import './styles/app.css';
 
-createApp(App).use(router).mount('#app');
+const apolloClient = new ApolloClient({
+    link: createHttpLink({
+        uri: 'https://maximedeoliveira-blog-api.herokuapp.com/graphql',
+    }),
+    cache: new InMemoryCache(),
+});
+
+const app = createApp({
+    setup() {
+        provide(DefaultApolloClient, apolloClient);
+    },
+    render: () => h(App),
+});
+
+app.use(router).mount('#app');
